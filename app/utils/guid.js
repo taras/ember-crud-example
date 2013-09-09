dd9b6e6c347caf559a9d889d8cbc445b773885a8
Taras Mankovski <tarasm@gmail.com>
2013-09-09 16:15:03 +0200

    Changed guid to variable from

--- app/utils/guid.js
+++ app/utils/guid.js
@@ -6,12 +6,12 @@
  * @link http://slavik.meltser.info/?p=142
  * @link http://stackoverflow.com/a/16693578/172894
  */
-function guid() {
+var guid = function() {
   function _p8(s) {
     var p = (Math.random().toString(16)+"000000000").substr(2,8);
     return s ? "-" + p.substr(0,4) + "-" + p.substr(4,4) : p ;
   }
   return _p8() + _p8(true) + _p8(true) + _p8();
-}
+};
 
 export default guid;

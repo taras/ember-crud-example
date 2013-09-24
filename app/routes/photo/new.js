c5a0ef1e3c01b81ca7000a936b78bcb7195d5cca
Taras Mankovski <tarasm@gmail.com>
2013-09-24 20:13:12 +0200

    Replaced bindAttr with bind-attr

--- app/routes/photo/new.js
+++ app/routes/photo/new.js
@@ -4,9 +4,6 @@ var PhotoNewRoute = Ember.Route.extend({
   model: function() {
     // provide a new photo to the template
     return Photo.create({});
-  },
-  setupController: function( controller, model ) {
-    controller.set( 'content', model );
   }
 });
 
